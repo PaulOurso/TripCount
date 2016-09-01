60a1fba967e5f19f193ce97b3a72caa3c6bc7be4
Tony Wisniewski <wisniewski.tony@gmail.com>
2016-09-02 01:08:43 +0200

    Add Serializable for all model

--- android/app/src/main/java/project/devmob/tripcount/models/Spending.java
+++ android/app/src/main/java/project/devmob/tripcount/models/Spending.java
@@ -4,6 +4,7 @@ import com.google.android.gms.common.internal.safeparcel.SafeParcelable;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.Serializable;
 import java.lang.reflect.Type;
 import java.util.Date;
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 /**
  * Created by Jean-Noel on 26/07/2016.
  */
-public class Spending {
+public class Spending implements Serializable {
     public String id;
     public String name;
     public double price;
