941d83f2cca6da0e3da6288e3424c34344f1520c
Tony Wisniewski <wisniewski.tony@gmail.com>
2016-09-07 00:51:43 +0200

    Add pull to refresh into Spending fragment

--- android/app/src/main/java/project/devmob/tripcount/ui/group/fragment/SpendingFragment.java
+++ android/app/src/main/java/project/devmob/tripcount/ui/group/fragment/SpendingFragment.java
@@ -3,6 +3,7 @@ package project.devmob.tripcount.ui.group.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,6 +30,7 @@ public class SpendingFragment extends Fragment {
     private ListView listViewSpending;
 
     private static final String TAG = "SpendingFrament" ;
+    private SwipeRefreshLayout swipeContainer;
 
     public SpendingFragment() {
         // Required empty public constructor
@@ -49,13 +51,35 @@ public class SpendingFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        refreshData();
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_spending, container, false);
+        listViewSpending = (ListView) view.findViewById(R.id.listing_spending);
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        swipeContainer.setColorSchemeResources(R.color.colorPrimary);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+        });
+
+        return view;
+    }
+
+    public void refreshData() {
         APIHelper.getSpendingsByGroupId(getContext(), myGroup, new TaskComplete<Type>() {
 
             @Override
             public void run() {
                 spendingList= (List<Spending>) this.result;
                 listViewSpending.setAdapter(new AdapterSpendingList(getContext(), R.layout.item_spending, spendingList));
+                swipeContainer.setRefreshing(false);
                 Log.d(TAG, ""+spendingList.size());
             }
         });
@@ -69,16 +93,5 @@ public class SpendingFragment extends Fragment {
             }
         });
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.fragment_spending, container, false);
-        listViewSpending = (ListView) view.findViewById(R.id.listing_spending);
-
-        return view;
-    }
-
 
 }
